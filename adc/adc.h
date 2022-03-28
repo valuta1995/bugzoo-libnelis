105f5e5c0ca2211b9f3bc3e8eda94ca27b3e3815
Glenn Visser <glenn.iain.visser@gmail.com>
2022-03-28 15:29:45 +0200

    Fixed adc readall

--- adc/adc.h
+++ adc/adc.h
@@ -8,15 +8,16 @@
 #define ADC_RESOLUTION                      (1<<MCP4728_RESOLUTION_BITS)
 
 struct adc_data_s {
-    uint16_t data[4];
-    float    val[4];
+    uint16_t data;
+    float    val;
 };
 
-extern adc_data_s adc_data;
+
+extern adc_data_s adc_data[4];
 
 void adc_init();
 uint16_t adc_read(uint8_t freedom_ain_channel);     // Get ADC conversion result for ain0 (ADC0_SE16), ain1 (ADC1_SE16), ain2 (ADC1_SE18) or ain3 (ADC1_SE23)
-void adc_readall();
+void adc_readall(adc_data_s* adc_data);
 
 
 #endif
